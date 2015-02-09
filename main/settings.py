cbd1a4082f5ced60ff6ea4ef25f23d9fb98b6fc5
Dylan <dgrayson11@gmail.com>
2015-02-09 09:45:46 -0700

    linked up static and templates in settings

--- main/settings.py
+++ main/settings.py
@@ -24,6 +24,8 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'main/templates')]
+
 ALLOWED_HOSTS = []
 
 
@@ -84,3 +86,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
