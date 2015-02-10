5120312267be15af8fe40f5bc1ffd632686a7447
Adam Thomas <adamthomas@Hobbes.local>
2015-02-09 19:49:11 -0700

    Began to wireup the urls.py

--- main/urls.py
+++ main/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 urlpatterns = patterns('',
 
 	url(r'^$', 'main.views.form.splash', name='splash'),
-    # Examples:
-    # url(r'^$', 'main.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^about', 'main.views.form.about', name='about'),
+    url(r'^campaign1', 'main.views.form.campaign1', name='campaign1'),
+    url(r'^campaign2', 'main.views.form.campaign2', name='campaign2'),
+    url(r'^campaign3', 'main.views.form.campaign3', name='campaign3'),
+    url(r'^admin/', include(admin.site.urls))
 )
