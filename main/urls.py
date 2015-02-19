e79621f29de1b39be422f86468cfcc1a9b677ad6
smashfligaeta <dgrayson11@gmail.com>
2015-02-19 16:54:22 -0700

    separated views from forms and switched nav links to use django url format

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-	url(r'^index', 'main.views.form.index', name='index'),
-	url(r'^$', 'main.views.form.splash', name='splash'),
-    url(r'^about', 'main.views.form.about', name='about'),
-    url(r'^campaigns', 'main.views.form.campaigns', name='campaigns'),
+	#url(r'^index', 'main.views.form.index', name='index'),
+	url(r'^$', 'main.views.views.splash', name='splash'),
+    url(r'^about', 'main.views.views.about', name='about'),
+    url(r'^campaigns', 'main.views.views.campaigns', name='campaigns'),
     url(r'^admin/', include(admin.site.urls))
 )
