6407e11bd548e42bc4b1c63ba91f6cc754c29c37
Adam 'Moody' Thomas <ajt235@nau.edu>
2015-02-19 17:23:55 -0700

    Fixed issues with the routing to the index and splash pages

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-	#url(r'^index', 'main.views.form.index', name='index'),
+	url(r'^index', 'main.views.form.index', name='index'),
 	url(r'^$', 'main.views.views.splash', name='splash'),
     url(r'^about', 'main.views.views.about', name='about'),
     url(r'^campaigns', 'main.views.views.campaigns', name='campaigns'),
