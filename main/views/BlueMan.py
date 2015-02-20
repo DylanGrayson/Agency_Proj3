8e1fb14af3bf60c1a157ba243fe2657662d86e54
Salvatore Bottiglieri <sb797@nau.edu>
2015-02-20 00:02:14 -0700

    added some photos to campaigns

---
+++ main/views/BlueMan.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class MoreInfoForm(forms.Form):
+	email = forms.EmailField(label="Your email")
+	name = forms.CharField(max_length=128)
+
+
+def Blue_Man(request):
+	if request.method == 'POST':
+		form = MoreInfoForm(request.POST)
+		if form.is_valid():
+			email = form.cleaned_data["email"]
+	else:
+		return render(request, "main/index.html", {})
